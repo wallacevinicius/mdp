32c67923b38b77233cc9b50d5b09e03cab6f337e
Bruno Correia <brunogomescorreia@gmail.com>
2018-02-08 17:52:49 -0300

    last version

--- upload.php
+++ upload.php
@@ -1,8 +1,14 @@
 <?php
 header('Content-type: text/json');
 $json = array();
+
+// dir random
+//$dir_random = md5(date('Y-m-d H:i:s.') . gettimeofday()['usec']) ;
+
+mkdir( 'uploads/' . $DIR_RANDOM , 0700 );
+
 // Pasta onde o arquivo vai ser salvo
-$_UP['pasta'] = 'uploads/';
+$_UP['pasta'] = 'uploads/' . $dir_random . '/';
 // Tamanho máximo do arquivo (em Bytes)
 $_UP['tamanho'] = 1024 * 1024 * 2; // 2Mb
 // Array com as extensões permitidas
@@ -16,7 +22,7 @@ $_UP['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML
 $_UP['erros'][3] = 'O upload do arquivo foi feito parcialmente';
 $_UP['erros'][4] = 'Não foi feito o upload do arquivo';
 // Verifica se houve algum erro com o upload. Se sim, exibe a mensagem do erro
-if ($_FILES['arquivo']['error'] != 0) {
+if ($_FILES['arquivo']['error'] != 0 && $_FILES['arquivo1']['error'] != 0) {
   $json['error'] = "Não foi possível fazer o upload, erro:" . $_UP['erros'][$_FILES['arquivo']['error']];
   echo json_encode($json);
   exit; // Para a execução do script
